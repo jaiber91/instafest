86e84b51238384b80542a7724e41c1384d2898d5
jaiber <jaiver.navia@gmail.com>
2022-12-23 13:50:26 -0500

    avance del home

---
+++ src/components/knownutility/Knowutility.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Knowutility.css";
+
+const Knowutility = () => {
+  return (
+    <section className="Knowutility">
+      <div className="Knowutility_first">
+        <h1>
+          conoce los <br /> utilitis nft´s <br /> inhands
+        </h1>
+        <div className="Knowutility_cards">
+          <div className="Knoeutility_card">
+            <h3>
+              ¿qué es <br /> un utility <br /> nft?
+            </h3>
+          </div>
+          <div className="Knoeutility_cardhow">
+            <h3>
+              ¿cómo <br /> funciona?
+            </h3>
+          </div>
+          <div className="Knoeutility_project">
+            <h3>
+              proyecto <br /> inhands
+            </h3>
+          </div>
+          <div className="Knoeutility_beneficios">
+            <h3>
+             beneficios
+            </h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Knowutility;
