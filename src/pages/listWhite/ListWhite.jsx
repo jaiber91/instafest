30d56c48eae5c5d72d2c62dd4a0c74a03d3d355c
jaiber <jaiver.navia@gmail.com>
2022-12-27 12:53:46 -0500

    avance del whitelist

--- src/pages/listWhite/ListWhite.jsx
+++ src/pages/listWhite/ListWhite.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ListWhite.css";
 import imageBg from "../../assets/images/landing/imagesBG.jpg";
 import logo from "../../assets/icons/logo.svg";
 import arrowUp from "../../assets/icons/arrow-up.svg";
 
 const ListWhite = () => {
+  // Definiendo el estado del componente
+  const [formData, setFormData] = useState([
+    { field: "Email", value: "" },
+    { field: "Nombre de usuario", value: "" },
+    { field: "Tipo de documento", value: "" },
+    { field: "Selecciona tu ubicación", value: "" },
+    { field: "Número de billetera virtual", value: "" },
+  ]);
+
+  // Índice del campo actual
+  const [currentFieldIndex, setCurrentFieldIndex] = useState(0);
+
+  // Función que maneja el evento de clic en el botón "Siguiente"
+  const handleNextClick = () => {
+    // Incrementa el índice del campo actual
+    const newFieldIndex = currentFieldIndex + 1;
+
+    // Si el índice del campo actual es mayor o igual al número de campos, establece el índice del campo actual en el último campo
+    if (newFieldIndex >= formData.length) {
+      setCurrentFieldIndex(formData.length - 1);
+      return;
+    }
+
+    setCurrentFieldIndex(newFieldIndex);
+
+    // Actualiza el estado del componente con los nuevos datos
+    setFormData(
+      formData.map((field, index) => {
+        if (index === currentFieldIndex) {
+          return {
+            ...field,
+            value: "",
+          };
+        }
+
+        return field;
+      })
+    );
+  };
+  // Función que maneja el evento de cambio en los campos de entrada
+  const handleChange = (event) => {
+    // Actualiza el estado del componente con el nuevo valor del campo
+    setFormData(
+      formData.map((field, index) => {
+        if (index === currentFieldIndex) {
+          return {
+            ...field,
+            value: event.target.value,
+          };
+        }
+
+        return field;
+      })
+    );
+  };
+
+  // Renderiza solo el campo actual
+  const currentField = formData[currentFieldIndex];
+
+  // Calcula el porcentaje de carga
+  const percentage = ((currentFieldIndex + 0) / formData.length) * 100;
+
+  // Si el porcentaje de carga es igual a 100%, muestra un mensaje de bienvenida
+  /*if (percentage === 100) {
+    return <h1>Bienvenido!</h1>;
+  }*/
+
   return (
     <section className="listWhite">
       <div className="listWhite_image">
@@ -14,19 +81,35 @@ const ListWhite = () => {
           <h3>Inhands</h3>
         </div>
         <div className="listWhite_Percentage">
-          <h2>0%</h2>
+          <h2>{percentage}%</h2>
         </div>
       </div>
       <div className="listWhite_form">
-        <h2>
-          la lista exclusiva para <br />
-          nuestros usuarios
-        </h2>
-        <p>Serás uno de los primeros en tener acceso a nuestras colecciones:</p>
-        <form className="listWhite_form_input" action="#">
-          <input type="email" name="emails" placeholder="E-mail" />
+        <div style={{ display: percentage >= 100 ? "none" : "block" }}>
+          <h2>
+            la lista exclusiva para <br />
+            nuestros usuarios
+          </h2>
+          <p>
+            Serás uno de los primeros en tener acceso a nuestras colecciones:
+          </p>
+        </div>
+
+        <div style={{ display: percentage >= 100 ? "block" : "none" }}>
+          <h1>¡Bienvenido/a!</h1>
+        </div>
+
+        <form className="listWhite_form_input">
+          <input
+            type="text"
+            name="emails"
+            id={currentField.field}
+            value={currentField.value}
+            onChange={handleChange}
+            placeholder={currentField.field}
+          />
           <hr />
-          <button>
+          <button type="button" onClick={handleNextClick}>
             <img src={arrowUp} alt="flecha" />
           </button>
         </form>
