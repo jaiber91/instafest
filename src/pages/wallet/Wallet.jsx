153dc0d160aa66d2c5d94e3484ad75581a7f6a94
jaiber <jaiver.navia@gmail.com>
2023-01-03 12:10:05 -0500

    vista de wallet terminada

--- src/pages/wallet/Wallet.jsx
+++ src/pages/wallet/Wallet.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Wallet.css";
 import imgWallet from "../../assets/images/wallet/wallet.svg";
-import logo from "../../assets/icons/logo.svg";
+import logo from "../../assets/icons/logo-white.svg";
 import Wallets from "../../components/wallets/Wallets";
 import logoMetamask from "../../assets/icons/metamask.svg";
 import logoCoinbase from "../../assets/icons/coinbase.svg";
@@ -11,7 +12,7 @@ import logoPhantom from "../../assets/icons/phantom.svg";
 import logoPortis from "../../assets/icons/portis.svg";
 
 const Wallet = () => {
-    /*Mostrando u ocultando la lista de wallets debajo del BTN  */
+  /*Mostrando u ocultando la lista de wallets debajo del BTN  */
   const [showDiv, setShowDiv] = useState(false);
 
   const handleButtonClick = () => {
@@ -19,22 +20,22 @@ const Wallet = () => {
   };
 
   /* Disparando modal al darle click a una de las wallets  */
-   const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const ButtonClickModal = () => {
     setShowModal(true);
   };
 
-
-
   return (
     <section className="wallet">
       <div className="wallet_image">
         <img src={imgWallet} alt="imagen" />
+        <Link to="/">
         <div className="wallet_logo">
           <img src={logo} alt="logo" />
           <h3>Inhands</h3>
         </div>
+        </Link>
       </div>
       <div className="wallet_list">
         <h2>
@@ -43,31 +44,83 @@ const Wallet = () => {
           <span>Utility NFT’s.</span>
         </h2>
         <div className="wallet_list_details">
-          <Wallets onClick={ButtonClickModal} imgLogo={logoCoinbase} nameWallet="Metamask" />
+          <Wallets
+            onClick={ButtonClickModal}
+            imgLogo={logoCoinbase}
+            nameWallet="Metamask"
+          />
 
-          <Wallets imgLogo={logoCoinbase} nameWallet="Coinbase" />
+          <Wallets
+            onClick={ButtonClickModal}
+            imgLogo={logoCoinbase}
+            nameWallet="Coinbase"
+          />
 
-          <Wallets imgLogo={walletConect} nameWallet="WalletConect" />
+          <Wallets
+            onClick={ButtonClickModal}
+            imgLogo={walletConect}
+            nameWallet="WalletConect"
+          />
         </div>
         <button
           type="button"
           onClick={handleButtonClick}
           className="wallet_list_btn"
         >
-           {showDiv ? 'Ver menos' : 'Ver más'} <img src={arrowRight} alt="flecha" />
+          {showDiv ? "Ver menos" : "Ver más"}{" "}
+          <img src={arrowRight} alt="flecha" />
         </button>
 
         {showDiv && (
           <div className="wallet_list_extra">
-            <Wallets imgLogo={logoPhantom} nameWallet="Phatom" />
-            <Wallets imgLogo={logoPortis} nameWallet="Portis" />
+            <Wallets
+              onClick={ButtonClickModal}
+              imgLogo={logoPhantom}
+              nameWallet="Phatom"
+            />
+            <Wallets
+              onClick={ButtonClickModal}
+              imgLogo={logoPortis}
+              nameWallet="Portis"
+            />
           </div>
         )}
+
+        <div className="wallet_container_btn">
+        <Link to="/colecciones">
+          <button>
+            <img src={arrowRight} alt="flecha" />
+          </button>
+          </Link>
+        </div>
       </div>
       {showModal && (
-        <div>
-          <p>Este es el contenido del modal</p>
-          <button onClick={() => setShowModal(false)}>Cerrar modal</button>
+        <div className="wallet_modal">
+          <div className="wallet_modal_element">
+            <div className="wallet_modal_logo">
+              <img src={logo} alt="logo" />
+              <h3>Inhands</h3>
+            </div>
+            <p>
+              Al conectar tu billetera y usar Inhands, <br /> estás de acuerdo
+              con nuestros
+              <span>
+                Términos de <br /> Servicio y Politica de Privacidad
+              </span>
+            </p>
+            <Link to="/whitelist">
+              <button className="wallet_modal_btn">
+                Aceptar y registrarme <img src={arrowRight} alt="flecha" />
+              </button>
+            </Link>
+
+            <button
+              className="wallet_modal_btnClose"
+              onClick={() => setShowModal(false)}
+            >
+              X
+            </button>
+          </div>
         </div>
       )}
     </section>
